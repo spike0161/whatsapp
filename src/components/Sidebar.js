7a3afdcbf4f90ac66ace6ecb7f81318f53304768
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-18 16:24:51 -0400

    Added material ui search bar and styled

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,33 @@
 import React from "react";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+import { Avatar, IconButton } from "@material-ui/core";
 import "../css/Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-header">
+        <Avatar />
         <div className="sidebar-header-right">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar-search">
+        <div className="sidebar-container">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
         </div>
       </div>
     </div>
