c48ff38fba0e82fd1581f144a9ae74b7eb806285
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-21 15:29:29 -0400

    Import ui icons to char component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import '../css/Chat.css'
+import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "/@material-ui/icons";
+import "../css/Chat.css";
 
 function Chat() {
   return (
     <div className="chat">
-    <h1>Chat component</h1>
+      <div className="chat-header">
+        <Avatar />
+
+        <div className="chat-header-info">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat-header-right">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
