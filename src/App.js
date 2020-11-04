3cca4df6ddceabb8dd713ce662a0357ccfca6636
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-04 09:53:51 -0500

    Setup alert on new message to db

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
 import './App.css';
+import Pusher from 'pusher-js'
 
 function App() {
+
+useEffect(() => {
+  var pusher = new Pusher('8ab09b5f25f4daadf04e', {
+        cluster: 'us2'
+      });
+
+      var channel = pusher.subscribe('messages');
+      channel.bind('inserted', function(data) {
+        alert(JSON.stringify(data));
+      });
+}, [])
+
   return (
     <div className="app">
     <div className="app-body">
