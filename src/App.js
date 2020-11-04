069b53169eb2412918d6849c7f4e9fcba89c7546
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-04 10:58:35 -0500

    fetch all messages from db to frontend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { useEffect } from 'react';
-import Sidebar from './components/Sidebar'
-import Chat from './components/Chat'
-import './App.css';
-import Pusher from 'pusher-js'
+import React, { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+import "./App.css";
+import Pusher from "pusher-js";
+import axios from "./axios.js";
 
 function App() {
+  // hook to set messages
+  const [messages, setMessages] = useState([]);
 
-useEffect(() => {
-  var pusher = new Pusher('8ab09b5f25f4daadf04e', {
-        cluster: 'us2'
-      });
+  // grabs all the messages the first time only
+  useEffect(() => {
+    axios.get("http://localhost:9000/messages/sync").then(response => {
+      setMessages(response.data)
+      console.log(response.data);
+    })
+  }, []);
 
-      var channel = pusher.subscribe('messages');
-      channel.bind('inserted', function(data) {
-        alert(JSON.stringify(data));
-      });
-}, [])
+  useEffect(() => {
+    const pusher = new Pusher("8ab09b5f25f4daadf04e", {
+      cluster: "us2",
+    });
 
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+  console.log(messages);
   return (
     <div className="app">
-    <div className="app-body">
-    <Sidebar />
-    <Chat />
-    </div>
+      <div className="app-body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
