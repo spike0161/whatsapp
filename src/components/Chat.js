011205e14c1daaf1ec2c159a4739b0d7e22de60f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-05 17:26:29 -0500

    Function to log user input in message text field

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
@@ -6,6 +6,12 @@ import MicIcon from "@material-ui/icons/Mic";
 import "../css/Chat.css";
 
 function Chat({ messages }) {
+  const [input, setInput] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="chat">
       <div className="chat-header">
@@ -42,8 +48,15 @@ function Chat({ messages }) {
       <div className="chat-footer">
         <InsertEmoticonIcon />
         <form>
-          <input type="text" placeholder="Type a message..." />
-          <button type="submit">Send a message</button>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Type a message..."
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
         </form>
         <MicIcon />
       </div>
